83cbb83836eca55de3dbc2cadd398e932406dc9c
Dmitry <voronov-d88@yandex.ru>
2018-02-05 09:54:14 +0000

    Render title and count of cinemas films

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,10 +1,13 @@
 from flask_app.flask_server import app, cache
 from flask import render_template
 import time
+import cinemas
 
 
 @app.route('/')
-@cache.cached(timeout=50)
+@cache.cached(timeout=5000)
 def films_list():
-    time.sleep(15)
-    return render_template('films_list.html')
+    afisha_raw_html = cinemas.fetch_afisha_page()
+    films = cinemas.parse_afisha_list(afisha_raw_html)
+    films = sorted(films, key=lambda f: f['count_cinema'], reverse=True)[:10]
+    return render_template('films_list.html', films=films)
