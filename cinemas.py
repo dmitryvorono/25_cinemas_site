b7329eede1fa6f8ea5d34d79559e05f53b2b9b63
Dmitry <voronov-d88@yandex.ru>
2018-02-12 14:42:45 +0000

    Add multithreading fetcher (untested)

--- cinemas.py
+++ cinemas.py
@@ -1,25 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
-import random
-import itertools
-import os
-import json
 import sys
-
-
-def load_fake_headers(filepath):
-    if not os.path.exists(filepath):
-        return None
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
-
-
-def load_proxies_list(filepath):
-    if not os.path.exists(filepath):
-        return None
-    with open(filepath, 'r') as proxies_file:
-        return proxies_file.read().split()
+import multitreading_kinopoisk_fetcher
 
 
 def fetch_afisha_page():
@@ -32,53 +15,6 @@ def parse_afisha_list(raw_html):
     afisha_html_tree = BeautifulSoup(raw_html, 'html.parser')
     film_elements = afisha_html_tree.find_all('div', {'class': 'object s-votes-hover-area collapsed'})
     return [{'title': f.find('h3').text, 'count_cinema': len(f.find_all('tr'))} for f in film_elements]
-
-
-def try_fetch_by_proxy(url, proxy, headers):
-    session = requests.session()
-    session.headers.update(headers)
-    session.proxies.update(proxy)
-    try:
-        request = session.get(url, timeout=10)
-    except:
-        return None
-    if request.status_code == requests.codes.ok:
-        return request.text
-
-
-def is_bad_html(raw_html):
-    if raw_html is None:
-        return True
-    movie_info_tree = BeautifulSoup(raw_html, 'html.parser')
-    capcha = movie_info_tree.find('img', {'class': 'image form__captcha'})
-    return bool(capcha)
-
-
-def fetch_movie_info(movie_title, cycle_proxy, fake_headers):
-    url = 'https://www.kinopoisk.ru/index.php?first=yes&what=&kp_query={0}'.format(movie_title)
-    movie_info = None
-    while movie_info is None:
-        proxies = {'https': next(cycle_proxy)}
-        headers = next(fake_headers)
-        movie_info = try_fetch_by_proxy(url, proxies, headers)
-        if is_bad_html(movie_info):
-            movie_info = None
-    return movie_info
-
-
-def get_html_text(html_tree, tag, attrs):
-    target_element = html_tree.find(tag, attrs)
-    if target_element is not None:
-        return target_element.text
-
-
-def parse_movie_info(raw_html):
-    if raw_html is None:
-        return {'rating': '0.0', 'count_votes': '0.0'}
-    movie_info_tree = BeautifulSoup(raw_html, 'html.parser')
-    rating = get_html_text(movie_info_tree, 'span', {'class': 'rating_ball'})
-    count_votes = get_html_text(movie_info_tree, 'span', {'class': 'ratingCount'})
-    return {'rating': rating, 'count_votes': count_votes}
 
 
 def filter_movies(movies, count_cinemas):
@@ -101,25 +37,14 @@ def create_parser():
     return parser
 
 
-def get_cycle_proxies(filepath):
-    proxies_list = load_proxies_list(filepath)
-    random.shuffle(proxies_list)
-    return itertools.cycle(set(proxies_list))
-
-
 def main():
     parser = create_parser()
     args = parser.parse_args()
     afisha_raw_html = fetch_afisha_page()
     films = parse_afisha_list(afisha_raw_html)
-    cycle_proxy = get_cycle_proxies('proxies.txt')
-    fake_headers = itertools.cycle(load_fake_headers('fake_headers.txt'))
     if args.cinema:
         films = filter_movies(films, args.cinema)
-    for f in films:
-        print('Fetch: {0}'.format(f['title']))
-        movie_info = fetch_movie_info(f['title'], cycle_proxy, fake_headers)
-        f.update(parse_movie_info(movie_info))
+    films = multitreading_kinopoisk_fetcher.fetch_raiting_films_in_kinopoisk(films)
     output_movies_to_console(films)
     return 0
 
--- multitreading_kinopoisk_fetcher.py
+++ multitreading_kinopoisk_fetcher.py
@@ -1,38 +1,102 @@
 import queue
 import threading
+import os
+import random
+import itertools
+import json
+import requests
+from bs4 import BeautifulSoup
 count_fetchers = 5
 
 
-def source():
-    return [{'n': 'a'}, {'n': 'b'}, {'n': 'c'}]
+def try_fetch_by_proxy(url, proxy, headers):
+    session = requests.session()
+    session.headers.update(headers)
+    session.proxies.update(proxy)
+    try:
+        request = session.get(url, timeout=10)
+    except:
+        return None
+    if request.status_code == requests.codes.ok:
+        return request.text
 
 
-def do_work(item):
-    item['r'] = 1
+def is_captcha_html(raw_html):
+    if raw_html is None:
+        return True
+    movie_info_tree = BeautifulSoup(raw_html, 'html.parser')
+    capcha = movie_info_tree.find('img', {'class': 'image form__captcha'})
+    return bool(capcha)
 
 
-def worker():
+def get_html_text(html_tree, tag, attrs):
+    target_element = html_tree.find(tag, attrs)
+    if target_element is not None:
+        return target_element.text
+
+
+def parse_movie_info(raw_html):
+    if raw_html is None:
+        return {'rating': '0.0', 'count_votes': '0.0'}
+    movie_info_tree = BeautifulSoup(raw_html, 'html.parser')
+    rating = get_html_text(movie_info_tree, 'span', {'class': 'rating_ball'})
+    count_votes = get_html_text(movie_info_tree, 'span', {'class': 'ratingCount'})
+    return {'rating': rating, 'count_votes': count_votes}
+
+
+def start_fetcher(film, proxy, headers):
+    url = 'https://www.kinopoisk.ru/index.php?first=yes&what=&kp_query={0}'.format(film['title'])
+    raw_kinopoisk_html = try_fetch_by_proxy(url, proxy, headers)
+    if is_captcha_html(raw_kinopoisk_html):
+        return film
+    film.update(parse_movie_info(raw_kinopoisk_html))
+
+
+def start_worker(queue_unprocessed_films, proxy, headers):
     while True:
-        item = queue_unprocessed_films.get()
-        if item is None:
+        film = queue_unprocessed_films.get()
+        if film is None:
             break
-        do_work(item)
+        start_fetcher(film, proxy, headers)
         queue_unprocessed_films.task_done()
 
-queue_unprocessed_films = queue.Queue()
-film_fetchers = [threading.Thread(target=worker) for i in range(count_fetchers)]
-for film_fetcher in film_fetchers:
-    film_fetcher.start()
-films = source()
-for item in films:
-    queue_unprocessed_films.put(item)
 
-# block until all tasks are done
-queue_unprocessed_films.join()
+def load_proxies_list(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as proxies_file:
+        return proxies_file.read().split()
 
-# stop workers
-for i in range(count_fetchers):
-    queue_unprocessed_films.put(None)
-for film_fetcher in film_fetchers:
-    film_fetcher.join()
-print(films)
+
+def get_cycle_proxies(filepath):
+    proxies_list = load_proxies_list(filepath)
+    random.shuffle(proxies_list)
+    return itertools.cycle(set(proxies_list))
+
+
+def load_fake_headers(filepath):
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
+
+
+def fetch_raiting_films_in_kinopoisk(films):
+    queue_unprocessed_films = queue.Queue()
+    cycle_proxy = get_cycle_proxies('proxies.txt')
+    cycle_fake_headers = itertools.cycle(load_fake_headers('fake_headers.txt'))
+    film_fetchers = [threading.Thread(target=start_worker,
+                                      args=(queue_unprocessed_films,
+                                            {'https': next(cycle_proxy)},
+                                            next(cycle_fake_headers)))
+                     for i in range(count_fetchers)]
+    for film_fetcher in film_fetchers:
+        film_fetcher.start()
+    for item in films:
+        queue_unprocessed_films.put(item)
+    queue_unprocessed_films.join()
+    for i in range(count_fetchers):
+        queue_unprocessed_films.put(None)
+    for film_fetcher in film_fetchers:
+        film_fetcher.join()
+    return films
