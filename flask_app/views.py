d5894fb208d6d78a7e11a62389b4b7fbe6ef3a6d
Dmitry <voronov-d88@yandex.ru>
2018-02-14 10:10:54 +0000

    Add save to database

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,7 +1,10 @@
-from flask_app.flask_server import app, cache
+from flask_app.flask_server import app, cache, db
+from flask_app import models
 from flask import render_template
 import time
 import cinemas
+import multitreading_kinopoisk_fetcher
+
 
 
 @app.route('/')
@@ -10,4 +13,28 @@ def films_list():
     afisha_raw_html = cinemas.fetch_afisha_page()
     films = cinemas.parse_afisha_list(afisha_raw_html)
     films = sorted(films, key=lambda f: f['count_cinema'], reverse=True)[:10]
+    films = multitreading_kinopoisk_fetcher.fetch_raiting_films_in_kinopoisk(films)
+    save_fetched_films(films)
     return render_template('films_list.html', films=films)
+
+
+def save_fetched_films(films):
+    for film in films:
+        if film['rating'] == '0.0':
+            continue
+        film_in_database = db.session.query(models.Film)
+        film_in_database = film_in_database.filter(models.Film.title == film['title'])
+        if film_in_database.count() > 0:
+            continue
+        new_film = create_film(film)
+        db.session.add(new_film)
+    db.session.commit()
+
+
+def create_film(film):
+    new_film = models.Film()
+    new_film.title = film['title']
+    new_film.count_cinema = film['count_cinema']
+    new_film.rating = film['rating']
+    new_film.image = film['image']
+    return new_film
