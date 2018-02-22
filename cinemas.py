cfdee7d34dae8b0ec3c7f676130bac960ff2495e
Dmitry <voronov-d88@yandex.ru>
2018-02-22 07:28:53 +0000

    Refactoring views

--- cinemas.py
+++ cinemas.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import argparse
-import sys
-import multitreading_kinopoisk_fetcher
+from multitreading_kinopoisk_fetcher import fetch_raiting_films_in_kinopoisk
+from flask_app.flask_server import db
+from flask_app.models import Film
+from config import COUNT_FILMS_ON_PAGE
 
 
 def fetch_afisha_page():
@@ -29,32 +30,58 @@ def sorted_movies(movies):
                   reverse=True)
 
 
-def output_movies_to_console(movies):
-    output_movies = sorted_movies(movies)
-    for movie in output_movies:
-        print('{0} {1} {2}'.format(movie['title'],
-                                   movie['rating'],
-                                   movie['image']))
+def get_saved_film(film):
+    film_in_database = db.session.query(Film)
+    film_in_database = film_in_database.filter(Film.title == film['title'])
+    if film_in_database.count() == 0:
+        return None
+    return film_in_database.first()
 
 
-def create_parser():
-    parser = argparse.ArgumentParser(description='This script generate rating films showing in Moscow. List cinemas get in afish.ru, rating - kinopoisk.ru')
-    parser.add_argument('-c', '--cinema', type=int, help='If input in rating will not include films with count_cinema less than input value')
-    return parser
+def save_fetched_films(films):
+    saved_films = []
+    for film in films:
+        if film['rating'] == '0.0':
+            continue
+        new_film = create_film(film)
+        db.session.add(new_film)
+        saved_films.append(new_film)
+    db.session.commit()
+    return saved_films
 
 
-def main():
-    parser = create_parser()
-    args = parser.parse_args()
+def delete_film_without_rating():
+    query_films = db.session.query(Film).filter(Film.rating.is_(None))
+    if query_films.count() == 0:
+        return 0
+    count_deleted_films = query_films.delete(synchronize_session=False)
+    db.session.commit()
+    return count_deleted_films
+
+
+def create_film(film):
+    new_film = Film()
+    new_film.title = film['title']
+    new_film.count_cinema = film['count_cinema']
+    new_film.rating = film['rating']
+    new_film.image = film['image']
+    return new_film
+
+
+def get_film_for_render():
     afisha_raw_html = fetch_afisha_page()
     films = parse_afisha_list(afisha_raw_html)
-    if args.cinema:
-        films = filter_movies(films, args.cinema)
-    films = multitreading_kinopoisk_fetcher.fetch_raiting_films_in_kinopoisk(films)
-    output_movies_to_console(films)
-    return 0
-
-
-if __name__ == '__main__':
-    status = main()
-    sys.exit(status)
+    films = sorted(films, key=lambda film: film['count_cinema'], reverse=True)
+    films = films[:COUNT_FILMS_ON_PAGE]
+    fetched_films = []
+    films_need_kinopoins_fetch = []
+    delete_film_without_rating()
+    for film in films:
+        saved_film = get_saved_film(film)
+        if saved_film:
+            fetched_films.append(saved_film)
+        else:
+            films_need_kinopoins_fetch.append(film)
+    films_need_kinopoins_fetch = fetch_raiting_films_in_kinopoisk(films_need_kinopoins_fetch)
+    saved_films = save_fetched_films(films_need_kinopoins_fetch)
+    return fetched_films + saved_films
--- flask_app/views.py
+++ flask_app/views.py
@@ -1,66 +1,12 @@
-from flask_app.flask_server import app, cache, db
-from flask_app.models import Film
+from flask_app.flask_server import app, cache
 from flask import render_template
 import cinemas
-from multitreading_kinopoisk_fetcher import fetch_raiting_films_in_kinopoisk
-from config import TIME_OUT_CACHE, COUNT_FILMS_ON_PAGE
+from config import TIME_OUT_CACHE
 
 
 @app.route('/')
 @cache.cached(timeout=TIME_OUT_CACHE)
 def films_list():
-    afisha_raw_html = cinemas.fetch_afisha_page()
-    films = cinemas.parse_afisha_list(afisha_raw_html)
-    films = sorted(films, key=lambda film: film['count_cinema'], reverse=True)
-    films = films[:COUNT_FILMS_ON_PAGE]
-    fetched_films = []
-    films_need_kinopoins_fetch = []
-    delete_film_without_rating()
-    for film in films:
-        saved_film = get_saved_film(film)
-        if saved_film:
-            fetched_films.append(saved_film)
-        else:
-            films_need_kinopoins_fetch.append(film)
-    films_need_kinopoins_fetch = fetch_raiting_films_in_kinopoisk(films_need_kinopoins_fetch)
-    saved_films = save_fetched_films(films_need_kinopoins_fetch)
+    films = cinemas.get_film_for_render()
     return render_template('films_list.html',
-                           films=fetched_films + saved_films)
-
-
-def get_saved_film(film):
-    film_in_database = db.session.query(Film)
-    film_in_database = film_in_database.filter(Film.title == film['title'])
-    if film_in_database.count() == 0:
-        return None
-    return film_in_database.first()
-
-
-def save_fetched_films(films):
-    saved_films = []
-    for film in films:
-        if film['rating'] == '0.0':
-            continue
-        new_film = create_film(film)
-        db.session.add(new_film)
-        saved_films.append(new_film)
-    db.session.commit()
-    return saved_films
-
-
-def delete_film_without_rating():
-    query_films = db.session.query(Film).filter(Film.rating == None)
-    if query_films.count() == 0:
-        return 0
-    count_deleted_films = query_films.delete(synchronize_session=False)
-    db.session.commit()
-    return count_deleted_films
-
-
-def create_film(film):
-    new_film = Film()
-    new_film.title = film['title']
-    new_film.count_cinema = film['count_cinema']
-    new_film.rating = film['rating']
-    new_film.image = film['image']
-    return new_film
+                           films=films)
